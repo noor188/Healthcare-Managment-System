e22f8d189ebfc9b2298f9b17f778a5d883cfe4a8
noor188 <noormmlk@gmail.com>
2025-02-02 20:17:47 -0700

    Ticket #2,#3,#5

--- src/main/java/com/healthcaremanagement/service/PatientService.java
+++ src/main/java/com/healthcaremanagement/service/PatientService.java
@@ -1,11 +1,8 @@
 package com.healthcaremanagement.service;
 
-import com.healthcaremanagement.model.Appointment;
 import com.healthcaremanagement.model.Doctor;
 import com.healthcaremanagement.model.Patient;
 import com.healthcaremanagement.repository.PatientRepositoryImpl;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -37,8 +34,8 @@ public class PatientService {
         patientRepository.deletePatient(id);
     }
 
-    public void removeDoctorForPatient(int patientId, Doctor doctor) {
-        this.patientRepository.removeDoctorForPatient(patientId, doctor);
+    public void removeDoctorFromPatient(int patientId, Doctor doctor) {
+        this.patientRepository.removeDoctorFromPatient(patientId, doctor);
     }
 
     public void addDoctorToPatient(int patientID, Doctor doctor) {
